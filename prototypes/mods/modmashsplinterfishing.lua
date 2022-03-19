69d8cf5f30349a500dbd51b0806d0ac5584fe27a
dslam127 <dslam127@gmail.com>
2022-03-19 12:32:43 -0400

    auto calculate ingredients, new dependency flib

--- prototypes/mods/modmashsplinterfishing.lua
+++ prototypes/mods/modmashsplinterfishing.lua
@@ -1,8 +1,10 @@
+local math = require("__flib__.math")
+
 -- add roe and fishfood to ingredient list, values vary based on water setting
 local value = settings.startup["xtreme-fishing-waterpercycle"].value
-local ingredients = {
+
+data.raw["recipe"]["af_fish"].ingredients = {
   { type = "fluid", name = "water", amount = value },
 	{ type = "item", name = "roe", amount = math.round(value / 25)},
 	{ type = "item", name = "fishfood", amount = math.round(value / 2.5)}
 }
-table.insert(data.raw["recipe"]["af_fish"].ingredients, ingredients)
